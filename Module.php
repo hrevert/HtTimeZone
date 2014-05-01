c3f85dc085e3d4f190e53974e02204d33c960726
Ujjwal Ojha <bring2uo@gmail.com>
2014-05-01 12:35:32 +0545

    Update to PSR-4 autoloader

--- Module.php
+++ Module.php
@@ -2,6 +2,6 @@
 /**
  * This file is placed here for compatibility with Zendframework 2's ModuleManager.
  * It allows usage of this module even without composer.
- * The original Module.php is in 'src/HtTimeZone' in order to respect PSR-0
+ * The original Module.php is in 'src/' in order to respect PSR-4
  */
-require_once __DIR__ . '/src/HtTimeZone/Module.php';
+require_once __DIR__ . '/src/Module.php';
--- src/Module.php
+++ src/Module.php
@@ -20,7 +20,7 @@ class Module implements
      */
     public function getConfig()
     {
-        return include __DIR__ . '/../../config/module.config.php';
+        return include __DIR__ . '/../config/module.config.php';
     }
 
     /**
@@ -29,9 +29,6 @@ class Module implements
     public function getAutoloaderConfig()
     {
         return [
-            'Zend\Loader\ClassMapAutoloader' => [
-                __DIR__ . '/../../autoload_classmap.php',
-            ],
             'Zend\Loader\StandardAutoloader' => [
                 'namespaces' => [
                     __NAMESPACE__ => __DIR__
